3eb2c84a28fe6c3b368d5545f043f57ee434db41
Geoffrey <gcloud@uncc.edu>
2026-06-19 15:32:23 -0400

    ingesting full dataset stored on S3

--- src/ingestion.py
+++ src/ingestion.py
@@ -4,14 +4,14 @@ from pathlib import Path
 
 # Setting up the path to the input file. In the final version, this will be an Amazon S3 link.
 PROJECT_ROOT = Path(__file__).resolve().parent.parent
-INPUT_FILE = PROJECT_ROOT / "data" / "sample.csv"
+INPUT_FILE = "s3://flight-records-2023-2026/T_ONTIME_REPORTING.csv"
 OUTPUT_DIR = PROJECT_ROOT / "stream_input"
 
 df = pd.read_csv(INPUT_FILE)
 
 os.makedirs(OUTPUT_DIR, exist_ok=True)
 
-chunk_size = 500 # Will change to 50000 for actual use
+chunk_size = 75000 # Will change to 75000 for actual use
 
 for i, start in enumerate(range(0, len(df), chunk_size)):
     chunk = df.iloc[start:start + chunk_size]
